60f1fa95cba17d2f798c13cbb60a8e72c3e310af
tirta71 <tirta4132@gmail.com>
2023-06-04 08:45:45 +0700

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 
 export default function HeroSection() {
+  useEffect(() => {
+    function socialBtnHover() {
+      $(".st-social-btn").hover(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+      });
+    }
+    socialBtnHover();
+  }, []);
+
   return (
-    <section class="st-hero-wrap st-parallax">
-      <div class="st-height-b80 st-height-lg-b80"></div>
-      <div class="st-hero st-style1 st-ripple-version">
-        <div class="container">
-          <div class="st-hero-text">
+    <section className="st-hero-wrap st-parallax">
+      <div className="st-height-b80 st-height-lg-b80"></div>
+      <div className="st-hero st-style1 st-ripple-version">
+        <div className="container">
+          <div className="st-hero-text">
             <h3>Hello, I’m</h3>
             <h1>
               Tirta <br />
               Samara
             </h1>
             <h2>Front end Web Developer</h2>
-            <div class="st-hero-btn">
+            <div className="st-hero-btn">
               <a
                 href="mailto:tirta@gmail.com"
-                class="st-btn st-style1 st-color1 st-smooth-move"
+                className="st-btn st-style1 st-color1 st-smooth-move"
               >
                 Hire me
               </a>
@@ -25,42 +35,46 @@ export default function HeroSection() {
         </div>
       </div>
       {/* <!-- Hero Image - Social Link Group --> */}
-      <div class="st-hero-img st-to-right">
-        <img class="wow fadeInRight" src="assets/img/hero-img.jpg" alt="Hero" />
-        <div class="st-social-group wow fadeInLeft">
-          <div class="st-social-link">
+      <div className="st-hero-img st-to-right">
+        <img
+          className="wow fadeInRight"
+          src="assets/img/hero-img.jpg"
+          alt="Hero"
+        />
+        <div className="st-social-group wow fadeInLeft">
+          <div className="st-social-link">
             <a
               href="https://www.instagram.com/tirtajasi/"
-              class="st-social-btn active"
+              className="st-social-btn active"
             >
-              <span class="st-social-icon">
-                <i class="fab fa-instagram"></i>
+              <span className="st-social-icon">
+                <i className="fab fa-instagram"></i>
               </span>
-              <span class="st-icon-name">Instagram</span>
+              <span className="st-icon-name">Instagram</span>
             </a>
             <a
               href="https://www.facebook.com/profile.php?id=100068785599390"
-              class="st-social-btn"
+              className="st-social-btn"
             >
-              <span class="st-social-icon">
-                <i class="fab fa-facebook"></i>
+              <span className="st-social-icon">
+                <i className="fab fa-facebook"></i>
               </span>
-              <span class="st-icon-name">Facebook</span>
+              <span className="st-icon-name">Facebook</span>
             </a>
-            <a href="https://twitter.com/tieerta" class="st-social-btn">
-              <span class="st-social-icon">
-                <i class="fab fa-twitter"></i>
+            <a href="https://twitter.com/tieerta" className="st-social-btn">
+              <span className="st-social-icon">
+                <i className="fab fa-twitter"></i>
               </span>
-              <span class="st-icon-name">Twitter</span>
+              <span className="st-icon-name">Twitter</span>
             </a>
             <a
               href="https://www.linkedin.com/in/tirta-samara-b84a48275/"
-              class="st-social-btn"
+              className="st-social-btn"
             >
-              <span class="st-social-icon">
-                <i class="fab fa-linkedin"></i>
+              <span className="st-social-icon">
+                <i className="fab fa-linkedin"></i>
               </span>
-              <span class="st-icon-name">LinkedIn</span>
+              <span className="st-icon-name">LinkedIn</span>
             </a>
           </div>
         </div>
